0689ab3d3af0280fb1e4d520c029d70fd8368a46
Martin <s73331@htw-dresden.de>
2017-06-07 22:57:43 +0200

    solved task 2 and refactored code

--- s73331.c
+++ s73331.c
@@ -16,21 +16,48 @@ void error()
 {
 	printf("An error occured.");
 }
-void usage(char *call_name)
+
+/*
+ *
+ */
+int decrypt_aes_128_ecb(unsigned char *cipher, int cipher_bytes, unsigned char *key, unsigned char *buf_plain, int *bytes_out)
 {
-	printf("error\n");
-	printf("usage: %s i\n", call_name);
-	printf("Replace i with one of the following numbers.\n");
-	printf("1. Prüfen Sie, zu welchem Chiffrat s73331-cipher01.bin, s73331-cipher02.bin oder s73331-cipher03.bin die Signatur s73331-sig.bin gehört. Nutzen Sie dazu den bereitgestellten öffentlichen Schlüssel pubkey.pem und das Hashverfahren SHA-1.\n");
-	printf("2. Entschlüsseln Sie das identifizierte Chiffrat mit Hilfe des Schlüssels s73331-key.bin. Falls ein Initialisierungsvektor nötig ist, so ist dieser im Anschluss an den Schlüssel in dieser Datei abgelegt. Der Klartext wurde mittels des Verfahrens AES-128-ECB verschlüsselt. (Das Verfahren wird durch die Funktion EVP_aes_128_ecb() der OpenSSL-Bibliothek implementiert). Der Klartext ist ein Dokument im PDF.\n");
-	printf("3. Bilden Sie einen kryptografischen Hash über dem korrekt entschlüsselten Klartext. Nutzen Sie das Verfahren EVP_md4(), das durch die Funktion der OpenSSL-Bibliothek implementiert wird. Speichern Sie den Hash in einer Datei s73331-hash.bin.\n");
+	EVP_CIPHER_CTX ctx;
+	if (EVP_DecryptInit(&ctx, EVP_aes_128_ecb(), key, NULL) == 0)
+		return -1;
+	if (EVP_DecryptUpdate(&ctx, buf_plain, bytes_out, cipher, cipher_bytes) == 0)
+		return -2;
+	return 1;
+}
+
+/*
+ * hashes message with sha1 into buf_hash
+ * return:
+ * 	       0: success
+ *     non-0: failure
+ */
+int sha1(unsigned char *message, int message_bytes, unsigned char *buf_hash)
+{
+	// hash
+	EVP_MD_CTX ctx;
+	if (EVP_DigestInit(&ctx, EVP_sha1()) == 0) {
+		return 1;
+	}
+	if (EVP_DigestUpdate(&ctx, message, message_bytes) == 0) {
+		return 2;
+	}
+	if (EVP_DigestFinal(&ctx, buf_hash, NULL) == 0) {
+		return 3;
+	}
+	return 0;
 }
 
 /*
  * FILE FUNCTIONS
  */
 /*
- * return value is number of bytes read
+ * return:
+ *     value is number of bytes read
  */
 int read_file(char *file_name, unsigned char *buf, int buf_size)
 {
@@ -55,6 +82,24 @@ int write_file(char *file_name, unsigned char *buf, int bytes_to_write)
 	fclose(file);
 	return bytes;
 }
+
+int decrypt_aes_128_ecb_file(char *cipher_file_name, char *key_file_name, unsigned char *buf_plain, int *bytes_out)
+{
+	unsigned char buf_cipher[BUF_SIZE];
+	int bytes = read_file(cipher_file_name, buf_cipher, sizeof(buf_cipher));
+	if (bytes == 0) {
+		return 0;
+	}
+
+	unsigned char buf_key[16];
+	int bytes_key = read_file(key_file_name, buf_key, sizeof(buf_key));
+	if (bytes_key == 0) {
+		return 0;
+	}
+
+	return decrypt_aes_128_ecb(buf_cipher, bytes, buf_key, buf_plain, bytes_out);
+}
+
 /*
  * Reads the file located at ./file_name,
  * calculates the sha1 hash,
@@ -70,25 +115,17 @@ int sha1_file(char *file_name)
 		return 0;
 	}
 
-	// hash
-	EVP_MD_CTX ctx;
-	if (EVP_DigestInit(&ctx, EVP_sha1()) == 0) {
-		return 0;
-	}
-	if (EVP_DigestUpdate(&ctx, buf, bytes) == 0) {
-		return 0;
-	}
 	unsigned char hash[SHA_DIGEST_LENGTH];
-	if (EVP_DigestFinal(&ctx, hash, NULL) == 0) {
+	if (sha1(buf, bytes, hash) == 0)
 		return 0;
-	}
 
 	// write to file
 	char new_file_name[BUF_SIZE];
 	strcpy(new_file_name, file_name);
 	strcat(new_file_name, "-sig.bin");
-	return write_file(new_file_name, hash, 20);
+	return write_file(new_file_name, hash, SHA_DIGEST_LENGTH);
 }
+
 
 /*
  * Verifies the digest file with the key file against the signature file
@@ -98,7 +135,7 @@ int sha1_file(char *file_name)
  * 0 Verification Failure
  * negative value: error
  */
-int rsa_verify_file(char *digest_file_name, char *key_file_name, char *signature_file_name)
+int rsa_verify_file(char *message_file_name, char *key_file_name, char *signature_file_name)
 {
 	FILE *key = fopen(key_file_name, "rb");
 	if (key == NULL)
@@ -110,53 +147,92 @@ int rsa_verify_file(char *digest_file_name, char *key_file_name, char *signature
 	int bytes_signature = read_file(signature_file_name, buf_signature, sizeof(buf_signature));
 	if (bytes_signature == 0)
 		return -2;
-	unsigned char buf_digest[BUF_SIZE];
-	int bytes_digest = read_file(digest_file_name, buf_digest, sizeof(buf_digest));
-	if (bytes_digest == 0)
+
+	unsigned char buf_message[BUF_SIZE];
+	int bytes_message = read_file(message_file_name, buf_message, sizeof(buf_message));
+	if (bytes_message == 0)
 		return -3;
-	int result = RSA_verify(NID_sha1, buf_digest, bytes_digest, buf_signature, bytes_signature, rsa);
+
+	unsigned char buf_digest[SHA_DIGEST_LENGTH];
+	if (sha1(buf_message, bytes_message, buf_digest) != 0)
+		return -4;
+
+	int result = RSA_verify(NID_sha1, buf_digest, sizeof(buf_digest), buf_signature, bytes_signature, rsa);
 	return result;
 }
 
+/*
+ * solution for tasks
+ */
+void task1(char *cipher_file_name_template, char *pubkey_file_name, char *signature_file_name, char *right_cipher_file_name)
+{
+	printf("\nAufgabe 1:\n");
+	int i;
+
+	for (i = 1; i < 4; i++) {
+		char cipher_file_name[BUF_SIZE];
+		sprintf(cipher_file_name, cipher_file_name_template, i);
+		int result = rsa_verify_file(cipher_file_name, pubkey_file_name, signature_file_name);
+		char output[BUF_SIZE];
+		if (result > 0) {
+			strcpy(output, "\tSignature did match for file ");
+			strcat(output, cipher_file_name);
+			strcpy(right_cipher_file_name, cipher_file_name);
+		} else if (result == 0) {
+			strcpy(output, "\tSignature did not match for file ");
+			strcat(output, cipher_file_name);
+		} else {
+			strcpy(output, "\tSignature check failed for file ");
+			strcat(output, cipher_file_name);
+		}
+		strcat(output, "\n");
+		printf(output);
+	}
+}
+
+void task2(char *right_cipher_file_name, char *key_file_name, unsigned char *buf_plain, int *bytes_out)
+{
+	printf("\nAufgabe 2:\n");
+	*bytes_out = 0;
+	int result = decrypt_aes_128_ecb_file(right_cipher_file_name, key_file_name, buf_plain, bytes_out);
+	char output[BUF_SIZE];
+	if (result == 1) {
+		strcpy(output, "\tSuccessfully decrypted cipher file ");
+		strcat(output, right_cipher_file_name);
+		strcat(output, "\n");
+	} else {
+		strcpy(output, "\tError when decrypting cipher file ");
+		strcat(output, right_cipher_file_name);
+		strcat(output, "\n");
+	}
+	printf(output);
+}
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		usage(argv[0]);
+	char *cipher_file_name_template = "s73331-cipher0%i.bin";
+	char *key_file_name = "s73331-key.bin";
+	char *pubkey_file_name = "pubkey.pem";
+	char *signature_file_name = "s73331-sig.bin";
+
+	char right_cipher_file_name[BUF_SIZE];
+	right_cipher_file_name[0] = 0;
+
+	task1(cipher_file_name_template, pubkey_file_name, signature_file_name, right_cipher_file_name);
+
+	if (right_cipher_file_name[0] == 0) {
+		printf("No signature matched.\nExiting.");
 		return 1;
 	}
-	//char buf[BUF_SIZE];
 
-	//read_file("s73331.c", buf, sizeof(buf));
+	printf("\nProgressing with cipher file %s\n", right_cipher_file_name);
 
-	if (sha1_file("s73331-cipher01.bin") == 0) {
-		error();
-		return 2;
+	unsigned char buf_plain[BUF_SIZE];
+	int bytes_out;
+	task2(right_cipher_file_name, key_file_name, buf_plain, &bytes_out);
+	if (bytes_out > 0) {
+		write_file("plain.pdf", buf_plain, bytes_out);
 	}
-	if (sha1_file("s73331-cipher02.bin") == 0) {
-		error();
-		return 3;
-	}
-	if (sha1_file("s73331-cipher03.bin") == 0) {
-		error();
-		return 4;
-	}
-	int result1 = rsa_verify_file("s73331-cipher01.bin-sig.bin", "pubkey.pem", "s73331-sig.bin");
-	if (result1 < 0) {
-		error();
-		return 5;
-	}
-	int result2 = rsa_verify_file("s73331-cipher02.bin-sig.bin", "pubkey.pem", "s73331-sig.bin");
-	if (result2 < 0) {
-		error();
-		return 6;
-	}
-	int result3 = rsa_verify_file("s73331-cipher03.bin-sig.bin", "pubkey.pem", "s73331-sig.bin");
-	if (result3 < 0) {
-		error();
-		return 7;
-	}
-	printf("Results for signature check:\n1: %i\n2: %i\n3: %i\n", result1, result2, result3);
-
+	printf("\n");
 	return 0;
 }
